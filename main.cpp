ef4f3ea62ad3e9fa24e5a4b7d36bc3276d7224ae
Patrick <pdf@80columns.com>
2020-08-12 22:25:23 -0700

    adding main driver cpp file

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "infinite.h"
+
+using namespace std;
+
+/*
+ * This is a driver file that demonstrates how to call each of the
+ * library methods
+ */
+
+int main(int argc, char** argv) {
+	// Addition
+	
+	
+	// Subtraction
+	
+
+	// Multiplication
+	
+
+	// Division
+
+
+
+	return 0;
+}
